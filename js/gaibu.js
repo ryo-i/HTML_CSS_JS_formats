fc04246397121dbf8eb2bb212ecfb3589a2982ec
イイダリョウ <iidazz0109@gmail.com>
2020-07-24 05:26:46 +0900

    loadイベント追加、コメント書式整理

--- js/gaibu.js
+++ js/gaibu.js
@@ -1,23 +1,28 @@
-//DOM
-var target = document.querySelector("#profile img");
+// イベントリスナ（DOM読み込み）
+window.addEventListener('load', function() {
 
-//関数定義
-function hakken () {
+    // 変数定義（DOM取得）
+    var target = document.querySelector("#profile img");
 
-    //配列
-    var serif = ["隠しボタンを見つけてくれてありがとう。私の名前は“Atama”です。",
-                 "何かご用はありませんか？", 
-                 "私を閉じますか？", 
-                 "本当に閉じますか？",
-                 "もう少しあなたとお話ししたいです。それでも閉じますか？",
-                 "……わかりました。ごきげんよう。"];
-    var len = serif.length; //（配列数）
+    // 関数定義
+    function hakken () {
 
-    //反復
-    for (var i =0; i < len; i++) {
-        window.alert(serif[i]); 
+        // 配列
+        var serif = ["隠しボタンを見つけてくれてありがとう。私の名前は“Atama”です。",
+                     "何かご用はありませんか？", 
+                     "私を閉じますか？", 
+                     "本当に閉じますか？",
+                     "もう少しあなたとお話ししたいです。それでも閉じますか？",
+                     "……わかりました。ごきげんよう。"];
+        var len = serif.length; //（配列数）
+
+        // 制御構造（反復）
+        for (var i =0; i < len; i++) {
+            window.alert(serif[i]); 
+        }
     }
-}
 
-//イベントリスナ
-target.addEventListener("click", hakken, false);
+    // イベントリスナ（クリック）
+    target.addEventListener("click", hakken, false);
+    
+});
